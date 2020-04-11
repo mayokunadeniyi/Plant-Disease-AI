16bef2f8173c5b3d1b676eb1f2f0fc021c32935e
mayokunthefirst <adeniyimayokun17@gmail.com>
2020-04-11 03:20:22 +0100

    Setup nav graph

---
+++ app/src/main/java/com/example/plantdiseaseai/ui/home/HomeFragment.kt
@@ -0,0 +1,25 @@
+package com.example.plantdiseaseai.ui.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.example.plantdiseaseai.R
+import com.example.plantdiseaseai.databinding.FragmentHomeBinding
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+}
---
+++ app/src/main/java/com/example/plantdiseaseai/ui/home/HomeFragmentViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.plantdiseaseai.ui.home
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * Created by Mayokun Adeniyi on 11/04/2020.
+ */
+
+class HomeFragmentViewModel : ViewModel(){
+
+}
