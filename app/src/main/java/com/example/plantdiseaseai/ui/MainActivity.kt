5ac8484c671555cb6ed833fb5df18b978b9df990
mayokunthefirst <adeniyimayokun17@gmail.com>
2020-04-11 03:18:37 +0100

    Create home fragment

---
+++ app/src/main/java/com/example/plantdiseaseai/ui/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.plantdiseaseai
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.plantdiseaseai.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        setupNavigation()
+    }
+
+    private fun setupNavigation() {
+        val navController = findNavController(R.id.mainNavFragment)
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp() = findNavController(R.id.mainNavFragment).navigateUp()
+}
