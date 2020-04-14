8f357f6ac14f340175169ca485965ced618f5a5a
mayokunthefirst <adeniyimayokun17@gmail.com>
2020-04-14 16:05:23 +0100

    Setup gallery fragment

--- app/src/main/java/com/example/plantdiseaseai/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/example/plantdiseaseai/ui/gallery/GalleryFragment.kt
@@ -1,11 +1,17 @@
 package com.example.plantdiseaseai.ui.gallery
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.example.plantdiseaseai.databinding.FragmentGalleryBinding
+import com.theartofdev.edmodo.cropper.CropImage
+import com.theartofdev.edmodo.cropper.CropImageView
+import timber.log.Timber
 
 /**
  * Created by Mayokun Adeniyi on 11/04/2020.
@@ -20,5 +26,29 @@ class GalleryFragment : Fragment(){
     ): View? {
         binding = FragmentGalleryBinding.inflate(layoutInflater)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.selectImage.setOnClickListener {
+            CropImage.activity()
+                .setGuidelines(CropImageView.Guidelines.ON)
+                .start(requireContext(),this)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE){
+            val result = CropImage.getActivityResult(data)
+            if (resultCode == RESULT_OK){
+                val resultUri = result.uri
+                Glide.with(this)
+                    .load(resultUri)
+                    .into(binding.selectImage)
+            }else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE){
+                val error = result.error
+                Timber.e(error)
+            }
+        }
     }
 }
