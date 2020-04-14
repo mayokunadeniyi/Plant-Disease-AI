1d64fb808b44358491e277c952d97f918250a6fc
mayokunthefirst <adeniyimayokun17@gmail.com>
2020-04-14 15:50:30 +0100

    Fix camera fragment

--- app/src/main/java/com/example/plantdiseaseai/ui/camera/CameraFragment.kt
+++ app/src/main/java/com/example/plantdiseaseai/ui/camera/CameraFragment.kt
@@ -1,8 +1,15 @@
 package com.example.plantdiseaseai.ui.camera
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.graphics.Matrix
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.util.Size
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -16,103 +23,124 @@ import androidx.camera.core.Preview
 import androidx.camera.core.PreviewConfig
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import com.example.plantdiseaseai.R
 import com.example.plantdiseaseai.databinding.FragmentCameraBinding
+import com.example.plantdiseaseai.utils.CAMERA_REQUEST_CODE
 import com.example.plantdiseaseai.utils.REQUEST_CODE_PERMISSIONS
 import com.example.plantdiseaseai.utils.REQUIRED_PERMISSIONS
 import com.google.android.material.snackbar.Snackbar
+import com.theartofdev.edmodo.cropper.CropImage
+import com.theartofdev.edmodo.cropper.CropImageActivity
+import com.theartofdev.edmodo.cropper.CropImageOptions
+import com.theartofdev.edmodo.cropper.CropImageView
+import timber.log.Timber
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class CameraFragment : Fragment() {
     private lateinit var binding: FragmentCameraBinding
+    private lateinit var currentPhotoPath: String
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentCameraBinding.inflate(inflater)
 
-        if (allPermissionsGranted()){
-            binding.viewFinder.post{ startCamera()}
-        }else{
-            ActivityCompat.requestPermissions(this.requireActivity(), REQUIRED_PERMISSIONS,
-                REQUEST_CODE_PERMISSIONS)
-        }
-
-        binding.viewFinder.addOnLayoutChangeListener { v, left, top, right, bottom, oldLeft, oldTop, oldRight, oldBottom ->
-            updateTransform()
-        }
-
-
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-    private fun startCamera(){
-        // Create configuration object for the viewfinder use case
-        val previewConfig = PreviewConfig.Builder().apply {
-            setTargetResolution(Size(640, 480))
-        }.build()
-
-
-        // Build the viewfinder use case
-        val preview = Preview(previewConfig)
-
-        // Every time the viewfinder is updated, recompute layout
-        preview.setOnPreviewOutputUpdateListener {
-
-            // To update the SurfaceTexture, we have to remove it and re-add it
-            val parent = binding.viewFinder.parent as ViewGroup
-            parent.removeView(binding.viewFinder)
-            parent.addView(binding.viewFinder, 0)
-
-            binding.viewFinder.surfaceTexture = it.surfaceTexture
-            updateTransform()
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(
+                this.requireActivity(), REQUIRED_PERMISSIONS,
+                REQUEST_CODE_PERMISSIONS
+            )
         }
-
-        // Bind use cases to lifecycle
-        CameraX.bindToLifecycle(viewLifecycleOwner, preview)
     }
 
-    private fun updateTransform(){
-        val matrix = Matrix()
+    private fun startCamera() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(requireActivity().packageManager)?.also {
+                val photoFile: File? = try {
+                    createImageFile()
+                } catch (ex: IOException) {
+                    Timber.e(ex)
+                    null
+                }
 
-        // Compute the center of the view finder
-        val centerX = binding.viewFinder.width / 2f
-        val centerY = binding.viewFinder.height / 2f
-
-        // Correct preview output to account for display rotation
-        val rotationDegrees = when(binding.viewFinder.display.rotation) {
-            Surface.ROTATION_0 -> 0
-            Surface.ROTATION_90 -> 90
-            Surface.ROTATION_180 -> 180
-            Surface.ROTATION_270 -> 270
-            else -> return
+                photoFile?.also {
+                    val photoUri: Uri = FileProvider.getUriForFile(
+                        requireContext(),
+                        "com.example.plantdiseaseai",
+                        it
+                    )
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri)
+                    startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE)
+                }
+            }
         }
-        matrix.postRotate(-rotationDegrees.toFloat(), centerX, centerY)
-
-        // Finally, apply transformations to our TextureView
-        binding.viewFinder.setTransform(matrix)
     }
+
+    private fun createImageFile(): File {
+        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
+        val imageFileName = "JPEG_" + timeStamp + "_"
+        val storageDir = this.requireContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        val image = File.createTempFile(
+            imageFileName,
+            ".jpg",
+            storageDir
+        )
+        currentPhotoPath = image.absolutePath
+        return image
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == CAMERA_REQUEST_CODE) {
+            Timber.i("Yes it is")
+            if (resultCode == Activity.RESULT_OK && data != null) {
+                val f = File(currentPhotoPath)
+                val uri = Uri.fromFile(f)
+                CropImage.activity(uri)
+                    .start(requireContext(),this)
+            }
+        }
+
+
+    }
+
+
 
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
         grantResults: IntArray
     ) {
-        if (requestCode == REQUEST_CODE_PERMISSIONS){
-            if (allPermissionsGranted()){
-                binding.viewFinder.post{startCamera()}
-            }else{
-                Snackbar.make(binding.root,"Permissions not granted by the user.",Snackbar.LENGTH_SHORT).show()
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                Snackbar.make(
+                    binding.root,
+                    "Permissions not granted by the user.",
+                    Snackbar.LENGTH_SHORT
+                ).show()
                 this.activity?.finish()
             }
         }
     }
 
     private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
-        ContextCompat.checkSelfPermission(requireContext(),it) == PackageManager.PERMISSION_GRANTED
+        ContextCompat.checkSelfPermission(requireContext(), it) == PackageManager.PERMISSION_GRANTED
     }
 
 }
