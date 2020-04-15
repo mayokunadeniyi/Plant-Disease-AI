017bd6c5c019767c5d210206638d6032321bdcc1
mayokunthefirst <adeniyimayokun17@gmail.com>
2020-04-15 01:19:17 +0100

    Add Classifier class

---
+++ app/src/main/java/com/example/plantdiseaseai/utils/Constants.kt
@@ -0,0 +1,11 @@
+package com.example.plantdiseaseai.utils
+
+import android.Manifest.*
+
+/**
+ * Created by Mayokun Adeniyi on 11/04/2020.
+ */
+
+const val REQUEST_CODE_PERMISSIONS = 10
+val REQUIRED_PERMISSIONS = arrayOf(permission.CAMERA,permission.WRITE_EXTERNAL_STORAGE)
+const val CAMERA_REQUEST_CODE = 101
